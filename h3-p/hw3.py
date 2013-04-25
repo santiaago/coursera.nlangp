fea2c20c63e60d46be911f0f1aa52f06682b1cb7
Santiaago <santiago.ariassar@gmail.com>
2013-04-25 23:40:00 +0200

    part 2 done

--- h3-p/hw3.py
+++ h3-p/hw3.py
@@ -3,7 +3,7 @@ import sys
 from collections import defaultdict
 
 VERBOSE = False
-
+DUMPALLITERATIONS = False
 def model1(filename_f, filename_e,S = 5):
     print 'model1'
     print '--------------------'
@@ -52,7 +52,6 @@ def model1(filename_f, filename_e,S = 5):
     if VERBOSE:
         print_counts(count)
         print_dict(words)
-    
 
     # initializing t
     print '------------------------------'
@@ -67,7 +66,6 @@ def model1(filename_f, filename_e,S = 5):
         print_t(t)
     
     c = defaultdict()
-    #delta = {}
     for s in range(1,S+1):
         print
         print 'iteration #%s'%s
@@ -78,54 +76,146 @@ def model1(filename_f, filename_e,S = 5):
             sys.stdout.flush()
             # mk lenght of foreign sentence
             mk = len(f[k-1].split())
-            #print f[k-1]
-            #print mk
             sentence_f = f[k-1].split()
             for i in range(1,mk+1):
                 # lk is the length of the english sentence
                 lk = len(e[k-1].split())
                 sentence_e = ['NULL']+e[k-1].split()
-                #print e[k-1]
-                #print lk
                 for j in range(0,lk+1):
-                    # delta
+
                     fik = sentence_f[i-1]
-                    #print sentence_e
-                    #print j
                     ejk = sentence_e[j]
                     sumt = 0
                     for m in range(lk+1):
                         emk = sentence_e[m]
                         sumt += t[fik][emk]
-                    #delta[(k,i,j)] = t[fik][ejk]/sumt
                     delta = t[fik][ejk]/sumt
                     if (ejk,fik) not in c:
                         c[(ejk,fik)] = 0
-                    c[(ejk,fik)] += delta#[(k,i,j)]
+                    c[(ejk,fik)] += delta
                     if (ejk,) not in c:
                         c[(ejk,)] = 0
-                    c[(ejk,)] += delta#[(k,i,j)]
+                    c[(ejk,)] += delta
                     if j not in c:
-                        c[(j,)]= defaultdict(float)#{}
+                        c[(j,)]= defaultdict(float)
                         c[(j,)][(i,lk,mk)] = 0 
                     else:
                         if (i,lk,mk) not in c[j]:
                             c[(j,)][(i,lk,mk)] = 0 
-                    c[(j,)][(i,lk,mk)] += delta#[(k,i,j)]
+                    c[(j,)][(i,lk,mk)] += delta
                     if (i,lk,mk) not in c:
                         c[(i,lk,mk)] = 0
-                    c[(i,lk,mk)] += delta#[(k,i,j)]
+                    c[(i,lk,mk)] += delta
         # set t(f|e) = c(e,f)/c(e)
         for w_en in words.keys():
             for w_f in words[w_en]:
                 t[w_f][w_en] = c[(w_en,w_f)]/float(c[(w_en,)])
-        
-        print '\ndumping iteration %s'%(s)
-        name = 'corpus.t_iteration%s.out'%(s)
-        dump_t(name,t)
-        #print_t(t)
-        #print_c(c)
+        if DUMPALLITERATIONS:
+            print '\ndumping iteration %s'%(s)
+            name = 'corpus.t_iteration%s.out'%(s)
+            dump_t(name,t)
+        if VERBOSE:
+            print_t(t)
+            print_c(c)
     return t
+
+def model2(filename_f, filename_e,t,S = 5):
+    print 'model2'
+    print '--------------------'
+    print 'starting IBM Model 2'
+    print
+    print 'number of iterations is %s'%(S)
+    print
+    print 'training corpus f: %s'%(filename_f)
+    print 'training corpus e: %s'%(filename_e)
+    
+    f = codecs.open(filename_f, "r", "utf-8").readlines()
+    e = codecs.open(filename_e, "r", "utf-8").readlines()
+    
+    n = sum(1 for line in f)
+    
+    print 'number of lines f: %s'%(n)
+    
+    # initialize q
+    q = defaultdict()
+    for k in range(1,n+1):
+        # mk lenght of foreign sentence
+        mk = len(f[k-1].split())
+        for i in range(1,mk+1):
+            # lk is the length of the english sentence
+            lk = len(e[k-1].split())
+            for j in range(0,lk+1):
+                if j not in q:
+                    q[j] = defaultdict(float)
+                q[j][(i,lk,mk)] = 1/float(lk+1)
+
+    c = defaultdict()
+    for s in range(1,S+1):
+        print
+        print 'iteration #%s'%s
+        #set counts to 0
+        c = defaultdict()
+        for k in range(1,n+1):
+            sys.stdout.write("\rWorking with line %s of %s" % (k,n+1) )
+            sys.stdout.flush()
+            # mk lenght of foreign sentence
+            mk = len(f[k-1].split())
+            sentence_f = f[k-1].split()
+            for i in range(1,mk+1):
+                # lk is the length of the english sentence
+                lk = len(e[k-1].split())
+                sentence_e = ['NULL']+e[k-1].split()
+                for j in range(0,lk+1):
+
+                    fik = sentence_f[i-1]
+                    ejk = sentence_e[j]
+                    # delta
+                    sumqt = 0
+                    for m in range(lk+1):
+                        emk = sentence_e[m]
+                        sumqt += t[fik][emk]*q[m][(i,lk,mk)]
+
+                    delta = (t[fik][ejk]*q[j][(i,lk,mk)])/sumqt
+
+                    # cs
+                    if (ejk,fik) not in c:
+                        c[(ejk,fik)] = 0
+                    c[(ejk,fik)] += delta
+
+                    if (ejk,) not in c:
+                        c[(ejk,)] = 0
+                    c[(ejk,)] += delta
+
+                    if j not in c:
+                        c[(j,)]= defaultdict(float)
+                        c[(j,)][(i,lk,mk)] = 0 
+                    else:
+                        if (i,lk,mk) not in c[j]:
+                            c[(j,)][(i,lk,mk)] = 0 
+                            
+                    c[(j,)][(i,lk,mk)] += delta
+                    if (i,lk,mk) not in c:
+                        c[(i,lk,mk)] = 0
+                    c[(i,lk,mk)] += delta
+        # set t(f|e) = c(e,f)/c(e)
+        for w_f in t:
+            for w_e in t[w_f]:
+                t[w_f][w_e] = c[(w_e, w_f)]/ float(c[(w_e,)])
+        # set q(j|i,l,m) = c(j|i,l,m)/c(i,l,m)
+        for kk in range(1,n+1):
+            mkk = len(f[kk-1].split())
+            for ii in range(1,mkk+1):
+                # lk is the length of the english sentence
+                lkk = len(e[kk-1].split())
+                for jj in range(0,lkk+1):
+                    q[jj][(ii,lkk,mkk)] = 1/float(lkk+1)
+                
+        if VERBOSE:
+            print_t(t)
+            print_c(c)
+    return t
+
+
 def alignments(t,filename_f,filename_e):
     print 'alignments'
     f = codecs.open(filename_f, "r", "utf-8").readlines()
@@ -230,7 +320,15 @@ def part1():
     #dump_align('alignment_dev_4.p1.out',a)
 
 def part2():
-    pass
+    #t = read_t('corpus.t_iteration2.out')
+    #t = model2('corpus.es','corpus.en',t,5)
+    #dump_t('corpus.t5.p2.out',t)
+    t = read_t('corpus.t5.p2.out')
+    #a = alignments(t,'dev.es','dev.en')
+    #dump_align('alignment_dev.p2.out',a)
+
+    a = alignments(t,'test.es','test.en')
+    dump_align('alignment_test.p2.out',a)
 def part3():
     pass
 def tests():
